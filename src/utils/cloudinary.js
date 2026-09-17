e34c39f9261f9b85fa66972d203dfc8679c03108
Jenish Rana <jenishrana380@gmail.com>
2026-09-18 02:55:40 +0530

    cloudinary,multer added

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,29 @@
+import {v2 as cloudinary} from 'cloudinary'
+
+import fs from 'fs'
+
+// Configuration
+cloudinary.config({ 
+    cloud_name:process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET // Click 'View API Keys' above to copy your API secret
+});
+
+const uploadOnCloudinary= async (localFilePath)=>{
+    try {
+        if(!localFilePath) return null;
+        //upload the file on cloudinary
+        const response= await cloudinary.uploader.upload(localFilePath,{
+            resource_type:'auto'
+        })
+        // file has been successfully uploaded
+        console.log("file has been uploaded successfully: ",response.url)
+        return response;
+    
+    } catch (error) {
+        fs.unlinkSync(localFilePath); //remove the locally saved temporary file as upload operation got failed
+        return null;
+    } 
+}
+
+export {uploadOnCloudinary}
