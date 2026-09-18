6e56d6848585b610c2976eb6cfa701e9dce5d41d
Jenish Rana <jenishrana380@gmail.com>
2026-09-19 02:07:51 +0530

    controller and routes configured

--- src/app.js
+++ src/app.js
@@ -16,4 +16,12 @@ app.use(express.static("public"))
 app.use(cookieParser())
 
 
+//import routes
+import userRouter from "./routes/user.routes.js"
+
+//routes declaration
+app.use('/api/v1/users',userRouter)
+
+//  http://localhost:8000/api/v1/users/register
+
 export {app}
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,11 +1,10 @@
 
 //      with promise then catch, try catch is below 
 const asyncHandler=(requestHandler)=>{
-    (req,res,next)=>{
+    return (req,res,next)=>{
         Promise.resolve(requestHandler(req,res,next)).catch((error)=>next(error))
     }
 }
-
 
 
 export {asyncHandler}
