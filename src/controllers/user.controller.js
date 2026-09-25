69dd052032a7d36b81d2d2666e3af3fc24deae7d
Jenish Rana <jenishrana380@gmail.com>
2026-09-25 23:49:24 +0530

    bug fixes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -266,7 +266,7 @@ const changeCurrentPassword= asyncHandler ( async (req,res)=>{
 const getCurrentUser = asyncHandler ( async (req,res)=>{
     return res
     .status(200)
-    .json(200, req.user , "current user feteched successfully")
+    .json(new ApiResponse(200, req.user , "current user feteched successfully"))
 })
 
 const updateAccountDetails = asyncHandler( async (req,res)=>{
